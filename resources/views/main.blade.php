e21f0417ddafd5ca7365e9e9e2b242048c0bce1d
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-06-19 16:41:35 +0900

    change: title、meta descriptionの内容変更

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,5 @@
-<title>Json Iysis - Jsonを見やすい図へ変換</title>
-<meta name="description" content="JSONデータを直感的な図に変換する「JSON Crack」は、複雑な構造を持つJSONを簡単に理解できるように可視化します。このツールは、Monacoエディターを使用しており、リアルタイムでの編集とグラフの更新が可能です。ダークモードとライトモードの切り替え、エラーの検出、グラフの向きの変更、そしてグラフを画像としてエクスポートする機能も備えています。JSON Crackは、ブログやプレゼンテーションでの使用に最適なツールです。" />
+<title>JSON Iysis - Jsonを見やすい図へ変換</title>
+<meta name="description" content="JSONデータを直感的な図に変換する「JSON Iysis」は、複雑な構造を持つJSONを簡単に理解できるように可視化します。このツールは、リアルタイムでの編集とグラフの更新が可能です。エラーの検出、グラフの向きの変更、そしてグラフを画像としてエクスポートする機能も備えています。JSON Iysisは、Web開発やプレゼンテーションでの使用に最適なツールです。" />
 
 <link rel="stylesheet" href="/style.css" >
 <script src="{{ asset('/index.js') }}"></script>

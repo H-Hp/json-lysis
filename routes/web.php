8ea9696810507603ecfcd8ff5ee4d4d9097f315a
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-06-24 16:42:18 +0900

    feat: sitemapルーティング通す

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/favicons/favicon.ico', function () {
 Route::get('/robots.txt', function () {
     return response()->file(public_path('robots.txt'));
 });
+Route::get('/sitemap.xml', function () {
+    return response()->file(public_path('sitemap.xml'));
+});
 
 
 Route::get('/', function () {

29b61e205ec830653ed31dc901988253b84558b2
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-06-26 11:34:43 +0900

    update:view作り込み

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,13 +4,66 @@ export function sayHello() {
 }
 
 
-export function t() {
-  const button = document.getElementById('change-btn');
+export function t() {  const button = document.getElementById('change-btn');
   button.addEventListener('click', function() {
     const json_text = document.getElementById('json-input-field').value;
     //alert('ボタンがクリックされました3！'+json_text);
     //document.evaluate('//*[@id="chat"]/div"]').innerHTML = '<div><h1>'+json_text+'</h1></div>';
-    document.getElementById('json-zu').innerHTML = '<div class"jsons_wrap" style="background-color: #5eabac; border: #87f4f6 1px;"><h1 style="color: #f59e0b;">'+json_text+'</h1></div>';
+    //document.getElementById('json-zu').innerHTML = '<div class"jsons_wrap" style="background-color: #5eabac; border: #87f4f6 1px;"><h1 style="color: #f59e0b;">'+json_text+'</h1></div>';
+    try {
+        const json = JSON.parse(json_text);
+        console.log(json_text); // JSON オブジェクトが出力されます
+
+
+        //配列に
+        const keys = [];
+        const values = [];
+        let i=1;
+        for (const key in json) {
+          console.log("ループ"+i+"回目");
+          keys.push(key);
+          values.push(json[key]);
+          console.log("Key:", key); // Output: Keys: ["key1", "key2", "key3"]
+          console.log("Value:", json[key]); // Output: Values: ["value1", "value2", "value3"]
+          i++;
+
+          //document.getElementById('json-zu').innerHTML = '<div class"jsons_wrap2" style="background-color: #5eabac; border: #87f4f6 1px;top:40'+i+'"><p style="color: #f59e0b;">'+key+': '+json[key]+'</p></div>';
+          //document.getElementById('json-zu').prepend(document.createElement('<div class"jsons_wrap" style="background-color: #5eabac; border: #87f4f6"><p style="color: white">'+key+': '+json[key]+'</p></div>'));
     
+          if (typeof json[key] === "object") {//さらにjsonを持っている場合
+            console.log("さらなるjson");
+            //const json2 = JSON.parse(json[key]);
+            for (const key2 in  json[key]) {
+              console.log("2Key:", key2); // Output: Keys: ["key1", "key2", "key3"]
+              console.log("2Value:", json[key][key2]); // Output: Values: ["value1", "value2", "value3"]
+
+              document.getElementById('json-zu').insertAdjacentHTML('beforeend', '<div class="jsons_wrap" style="border: #87f4f6;position:relative;left:300px;"><p style="color: white;">' + key2 + ': ' + json[key][key2] + '</p></div>');
+
+              if (typeof json[key][key2] === "object") {
+                    console.log("さらなるさらなるjson");
+                  for (const key3 in  json[key][key2]) {
+                    console.log("3Key:", key3); // Output: Keys: ["key1", "key2", "key3"]
+                    console.log("3value:", json[key][key2][key3]); // Output: Values: ["value1", "value2", "value3"]
+                    document.getElementById('json-zu').insertAdjacentHTML('beforeend', '<div class="jsons_wrap" style="border: #87f4f6;position:relative;left:600px;"><p style="color: white;">' + key3 + ': ' + json[key][key2][key3] + '</p></div>');
+
+                  }
+
+                //}else {
+                  //document.getElementById('json-zu').insertAdjacentHTML('beforeend', '<div class="jsons_wrap" style="border: #87f4f6;position:relative;left:300px;"><p style="color: white;">' + key2 + ': ' + json[key][key2] + '</p></div>');
+                //}
+
+              }
+            }
+            
+          }else {
+            document.getElementById('json-zu').insertAdjacentHTML('beforeend', '<div class="jsons_wrap" style="border: #87f4f6"><p style="color: white;"><span class="key">' + key + '</span>: ' + json[key] + '</p></div>');
+          }
+        }
+
+        console.log("Keys:", keys); // Output: Keys: ["key1", "key2", "key3"]
+        console.log("Values:", values); // Output: Values: ["value1", "value2", "value3"]
+    } catch (error) {
+        console.error('JSON パースエラー:', error);
+    }
   });
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,34 +1,32 @@
 <title>JSON Iysis - Jsonを見やすい図へ変換</title>
 <meta name="description" content="JSONデータを直感的な図に変換する「JSON Iysis」は、複雑な構造を持つJSONを簡単に理解できるように可視化します。このツールは、リアルタイムでの編集とグラフの更新が可能です。エラーの検出、グラフの向きの変更、そしてグラフを画像としてエクスポートする機能も備えています。JSON Iysisは、Web開発やプレゼンテーションでの使用に最適なツールです。" />
 <link rel="shortcut icon" href="/favicons/favicon.ico">
-
 <!--
-
-
 <link rel="stylesheet" href="{{ asset('/build/asset/app.css') }}" >
 <script src="{{ asset('/build/asset/app.js') }}"></script>
 -->
-
-
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Googleアナリティクストラッキング tag (gtag.js) --><script async src="https://www.googletagmanager.com/gtag/js?id=G-N4Z9LH5GT2"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-N4Z9LH5GT2');</script>
+
+
 
 <div class="flex flex-col justify-between h-screen bg-gray-800 p-2 mx-auto max-w-full">
   <header class="flex items-center justify-center text-gray-200 text-2xl my-5">
     <div class="text-4xl ml-3 mr-3">JSON Iysis - Jsonを見やすい図へ変換</div>
   </header>
-  <a class="fixed left-4 top-4 md:right-14 md:top-6 text-xl text-white" href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fpinecone-io%2Fpinecone-vercel-starter&amp;env=OPENAI_API_KEY,PINECONE_API_KEY,PINECONE_CLOUD,PINECONE_REGION,PINECONE_INDEX&amp;envDescription=API%20Keys%20needed%20to%20run%20the%20application&amp;envLink=https%3A%2F%2Fdocs.pinecone.io%2Fdocs%2Fprojects%23api-keys&amp;project-name=my-awesome-pinecone-vercel-project&amp;repository-name=my-awesome-pinecone-vercel-project&amp;demo-title=Pinecone%20%2B%20Vercel%20AI%20SDK%20Starter&amp;demo-description=A%20Next.js%20starter%20chatbot%20using%20Vercel's%20AI%20SDK%20and%20implements%20the%20Retrieval-Augmented%20Generation%20(RAG)%20pattern%20with%20Pinecone&amp;demo-url=https%3A%2F%2Fpinecone-vercel-example.vercel.app%2F&amp;demo-image=https%3A%2F%2Fvercel.com%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fimages.ctfassets.net%252Fe5382hct74si%252F1G4xSqx0bCgVVv3aY3rrX4%252Ffa27791c39ddf058995561d794a68710%252FCleanShot_2023-07-21_at_11.55.49.png%26w%3D3840%26q%3D75%26dpl%3Ddpl_5bh93Tz7wfj1PdxgzMGwNCc1nAxA">
+  <a class="fixed left-4 top-4 md:right-14 md:top-6 text-xl text-white" href="/">
     <img src="https://vercel.com/button" alt="Deploy with Vercel">
   </a>
-  <button class="fixed right-12 top-4 md:right-12 md:top-6 text-xl text-white github-button">
-    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-    <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z">
-    </path>
-  </svg>
-</button>
+
+    <a class="fixed left-4 top-4 md:right-14 md:top-6 text-xl text-white" href="https://github.com/H-Hp/json-lysis">
+        <button class="fixed right-12 top-4 md:right-12 md:top-6 text-xl text-white github-button">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
+            </svg>
+        </button>
+    </a>
+
 <button class="fixed right-4 top-4 md:right-6 md:top-6 text-xl text-white animate-pulse-once info-button">
   <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
     <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
@@ -90,7 +88,9 @@
   </div>
               
           <div class="flex-grow w-full px-4">
-            <button id="change-btn"  class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-600 font-semibold text-zinc-100 hover:bg-zinc-400 active:bg-zinc-800 active:text-zinc-100/70 w-full my-2 uppercase active:scale-[98%] transition-transform duration-100" style="background-color:#4f6574;color:white">Clear Index</button>
+            <button id="change-btn"  class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-600 font-semibold text-zinc-100 hover:bg-zinc-400 active:bg-zinc-800 active:text-zinc-100/70 w-full my-2 uppercase active:scale-[98%] transition-transform duration-100" style="background-color:#4f6574;color:white">
+                jsonを図に変換
+            </button>
           </div>
           <div class="flex p-2"></div>
           <div class="text-left w-full flex flex-col rounded-b-lg bg-gray-600 p-3 subpixel-antialiased">
@@ -104,11 +104,55 @@
           </div>
         </div>
         <div class="flex flex-wrap w-full"></div>
+
+<!--
+        {
+          "key1" : "value1",
+          "key2" : "value2",
+          "key3" : "value3"
+        }
+      -->
 <textarea name="" id="json-input-field" class="json-input-field">
 {
-  "key1" : "value1",
-  "key2" : "value2",
-  "key3" : "value3"
+  "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
 }
 </textarea>
       </div>
@@ -116,3 +160,6 @@
     <button type="button" class="absolute left-20 transform -translate-x-12 bg-gray-800 text-white rounded-l py-2 px-4 lg:hidden">☰</button>
   </div>
 </div>
+
+
+

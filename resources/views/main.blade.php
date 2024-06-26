35b3cf7952a54a06510688c9c38d9d7196a9ef79
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-06-26 11:43:48 +0900

    update:必要ないレイアウトを削る

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -15,9 +15,6 @@
   <header class="flex items-center justify-center text-gray-200 text-2xl my-5">
     <div class="text-4xl ml-3 mr-3">JSON Iysis - Jsonを見やすい図へ変換</div>
   </header>
-  <a class="fixed left-4 top-4 md:right-14 md:top-6 text-xl text-white" href="/">
-    <img src="https://vercel.com/button" alt="Deploy with Vercel">
-  </a>
 
     <a class="fixed left-4 top-4 md:right-14 md:top-6 text-xl text-white" href="https://github.com/H-Hp/json-lysis">
         <button class="fixed right-12 top-4 md:right-12 md:top-6 text-xl text-white github-button">
@@ -40,51 +37,15 @@
       <div>
       </div>
     </div>
-    <form class="mt-5 mb-5 relative bg-gray-700 rounded-lg">
-      <input type="text" class="input-glow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline pl-3 pr-10 bg-gray-600 border-gray-600 transition-shadow duration-200" value="">
-      <span class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none text-gray-400">Press ⮐ to send</span>
-    </form>
+
   </div>
   <div class="absolute transform translate-x-full transition-transform duration-500 ease-in-out right-0 w-2/3 h-full bg-gray-700 overflow-y-auto lg:static lg:translate-x-0 lg:w-2/5 lg:mx-2 rounded-lg">
     <div class="flex flex-col border-2 overflow-y-auto rounded-lg border-gray-500 w-full ">
       <div class="flex flex-col items-start sticky top-0 w-full">
         <div class="flex flex-col items-start lg:flex-row w-full lg:flex-wrap p-2">
-          <div class="">
-            <div class="pr-2 lg:flex-grow">
-              <button class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-600 font-semibold text-zinc-100 hover:bg-zinc-400 active:bg-zinc-800 active:text-zinc-100/70 relative overflow-hidden w-full my-1 lg:my-2 mx-2 " style="background-color:bg-gray-800;color:text-gray-200">
-                <a href="https://e360.yale.edu/digest/indonesia-malaysia-deforestation" target="_blank" rel="noopener noreferrer" class="bg-blue-500 hover:bg-blue-600 active:bg-brand-700 dark:bg-brand-400 dark:hover:bg-brand-300 dark:active:bg-brand-200 flex items-center justify-center rounded-full p-2 text-3xl text-white transition duration-200 hover:cursor-pointer dark:text-white">
-                  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" color="white" font-size="14" class="hover:text-green" style="color:white" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M574 665.4a8.03 8.03 0 0 0-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 0 0-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 0 0 0 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 0 0 0 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 0 0-11.3 0L372.3 598.7a8.03 8.03 0 0 0 0 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z"></path>
-                  </svg>
-                </a>
-                <div class="relative">Indonesia Deforestation</div>
-              </button>
-            </div>
-          </div>
-          <div class="">
-            <div class="pr-2 lg:flex-grow">
-              <button class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-600 font-semibold text-zinc-100 hover:bg-zinc-400 active:bg-zinc-800 active:text-zinc-100/70 relative overflow-hidden w-full my-1 lg:my-2 mx-2 " style="background-color:bg-gray-800;color:text-gray-200">
-                <a href="https://cleantechnica.com/2023/06/29/solar-82-of-power-capacity-growth-in-india-in-2022/" target="_blank" rel="noopener noreferrer" class="bg-blue-500 hover:bg-blue-600 active:bg-brand-700 dark:bg-brand-400 dark:hover:bg-brand-300 dark:active:bg-brand-200 flex items-center justify-center rounded-full p-2 text-3xl text-white transition duration-200 hover:cursor-pointer dark:text-white">
-                  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" color="white" font-size="14" class="hover:text-green" style="color:white" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-              <path d="M574 665.4a8.03 8.03 0 0 0-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 0 0-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 0 0 0 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 0 0 0 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 0 0-11.3 0L372.3 598.7a8.03 8.03 0 0 0 0 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z"></path>
-            </svg>
-          </a>
-          <div class="relative">Solar Power in India</div>
-        </button>
-      </div>
-    </div>
-    <div class="">
-      <div class="pr-2 lg:flex-grow">
-        <button class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-600 font-semibold text-zinc-100 hover:bg-zinc-400 active:bg-zinc-800 active:text-zinc-100/70 relative overflow-hidden w-full my-1 lg:my-2 mx-2 " style="background-color:bg-gray-800;color:text-gray-200">
-          <a href="https://www.cbssports.com/nba/news/matisse-thybulle-to-stay-with-blazers-who-match-mavericks-three-year-33-million-deal-per-report/" target="_blank" rel="noopener noreferrer" class="bg-blue-500 hover:bg-blue-600 active:bg-brand-700 dark:bg-brand-400 dark:hover:bg-brand-300 dark:active:bg-brand-200 flex items-center justify-center rounded-full p-2 text-3xl text-white transition duration-200 hover:cursor-pointer dark:text-white">
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" color="white" font-size="14" class="hover:text-green" style="color:white" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-              <path d="M574 665.4a8.03 8.03 0 0 0-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 0 0-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 0 0 0 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 0 0 0 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 0 0-11.3 0L372.3 598.7a8.03 8.03 0 0 0 0 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z"></path>
-            </svg>
-          </a>
-          <div class="relative">Matisee Thybulle</div>
-        </button>
-      </div>
-    </div>
+
+   
+
   </div>
               
           <div class="flex-grow w-full px-4">
@@ -93,18 +54,8 @@
             </button>
           </div>
           <div class="flex p-2"></div>
-          <div class="text-left w-full flex flex-col rounded-b-lg bg-gray-600 p-3 subpixel-antialiased">
-            <label for="splittingMethod" class="text-white p-2 font-bold">Splitting Method:</label>
-            <div class="relative w-full">
-              <select id="splittingMethod" class="p-2 bg-gray-700 rounded text-white w-full appearance-none hover:cursor-pointer">
-                <option value="recursive">Recursive Text Splitting</option>
-                <option value="markdown" selected="">Markdown Splitting</option>
-              </select>
-            </div>
-          </div>
         </div>
         <div class="flex flex-wrap w-full"></div>
-
 <!--
         {
           "key1" : "value1",

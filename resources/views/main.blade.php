6a808175725002253060e23a64aff4cabf6c7847
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-07-17 22:37:07 +0900

    feat: schema.org･構造化ﾃﾞｰﾀ設定

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,6 +10,24 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Googleアナリティクストラッキング tag (gtag.js) --><script async src="https://www.googletagmanager.com/gtag/js?id=G-N4Z9LH5GT2"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-N4Z9LH5GT2');</script>
 
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "SoftwareApplication",
+  "name": "JSON Iysis - Jsonを見やすい図へ変換",
+  "description": "JSONデータを直感的な図に変換する「JSON Iysis」は、複雑な構造を持つJSONを簡単に理解できるように可視化します。このツールは、リアルタイムでの編集とグラフの更新が可能です。エラーの検出、グラフの向きの変更、そしてグラフを画像としてエクスポートする機能も備えています。JSON Iysisは、Web開発やプレゼンテーションでの使用に最適なツールです。",
+  "applicationCategory": "WebApplication",
+  "offers": {
+    "@type": "Offer",
+    "price": "0",
+    "priceCurrency": "JPY"
+  },
+  "creator": {
+    "@type": "Person",
+    "name": "RollTall"
+  }
+}
+</script>
 
 
 <div class="flex flex-col justify-between h-screen bg-gray-800 p-2 mx-auto max-w-full">

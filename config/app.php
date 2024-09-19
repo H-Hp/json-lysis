12ecdb03b3e807f4719f7688e48f56d237de9d81
H-Hp <stepjump3333@yahoo.co.jp>
2024-09-19 17:06:23 +0900

    feat: corsのためにulrをwwwなしにする

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://www.json-lysis.xyz'),
+    'url' => env('APP_URL', 'https://json-lysis.xyz'),
 
     'asset_url' => env('ASSET_URL'),
 
